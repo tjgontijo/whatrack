75479f7d56af751cb1b9d3b62be5acb1a758c13d
Thiago Gontijo <tjgontijo@gmail.com>
2026-03-13 18:16:23 -0300

    Create page.tsx

---
+++ src/app/dashboard/settings/webhooks/whatsapp/page.tsx
@@ -0,0 +1,10 @@
+import { WebhooksView } from '@/components/dashboard/whatsapp/settings/webhooks-view'
+import { TemplateMainShell } from '@/components/dashboard/leads'
+
+export default function WebhooksPage() {
+  return (
+    <TemplateMainShell>
+      <WebhooksView />
+    </TemplateMainShell>
+  )
+}
