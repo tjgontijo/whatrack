d0d4d9500318baba44716e411e1d24453a26fe8b
Thiago Gontijo <tjgontijo@gmail.com>
2026-03-13 18:16:24 -0300

    Delete page.tsx

--- src/app/dashboard/settings/whatsapp/webhooks/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import React from 'react'
-import { WebhooksView } from '@/components/dashboard/whatsapp/settings/webhooks-view'
-
-export default function WebhooksPage() {
-  return <WebhooksView />
-}
