1b13b9afe4480e508bd7e08425f9ec77eca1c120
Thiago Gontijo <tjgontijo@gmail.com>
2026-05-18 19:54:52 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   cacheComponents: true,
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
 
   images: {
     remotePatterns: [
